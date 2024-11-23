ae6a75e86f0f27fcef3b00fb972e3cfa3407683f
2bsuetam <00001114483837sp@al.educacao.sp.gov.br>
2024-11-23 17:00:12 -0300

    Add files via upload

---
+++ flashcard bp/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'BLACKPINK',
+    'Em que ano o BLACKPINK debutou?',
+    '2016'
+)
+
+criaCartao(
+    'BLACKPINK',
+    'Quem é a integrante mais velha do BLACKPINK?',
+    'Jisso.'
+)
+
+criaCartao(
+    'BLACKPINK',
+    'Qual é o apelido do fandom do BLACKPINK?',
+    'BLINK.'
+)
+
+criaCartao(
+    'BLACKPINK',
+    'Quem foi a primeira integrante a lançar uma música solo?',
+    'Jennie (SOLO).'
+)
